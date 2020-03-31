a8a5a8d3a09d19535fef5e0d75fe7b6ef89d6b90
pablo <menpablo@hotmail.com>
2020-03-31 14:36:16 -0300

    Fix

--- src/app/Models/ExternalIdAux.php
+++ src/app/Models/ExternalIdAux.php
@@ -10,6 +10,6 @@ class ExternalIdAux extends Model
     protected $table = 'external_id_aux';
 
     protected $fillable = [
-        'external_id','object_type','integration_system_id'
+         'external_id','object_type','integration_system_id'
     ];
 }
